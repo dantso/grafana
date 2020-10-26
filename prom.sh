a888e2eeb04b25342cf1292e5559ec80793589d6
dantso <35578467+dantso@users.noreply.github.com>
2020-10-26 10:45:08 -0400

    Delete prom.sh

--- prom.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo -u prometheus /usr/local/bin/prometheus --config.file /etc/prometheus/prometheus.yml --storage.tsdb.path /var/lib/prometheus/ --web.console.templates=/etc/prometheus/consoles --web.console.libraries=/etc/prometheus/console_libraries
