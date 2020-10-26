4524f4f56fd0e4e7ac0c3ae702fa744ff7b5fac5
dantso <35578467+dantso@users.noreply.github.com>
2020-10-26 10:47:53 -0400

    Create prometheus.sh

---
+++ prometheus/prometheus.sh
@@ -0,0 +1,41 @@
+curl -LO https://github.com/prometheus/prometheus/releases/download/v2.21.0/prometheus-2.21.0.linux-amd64.tar.gz
+tar xvf prometheus-2.21.0.linux-amd64.tar.gz
+sudo cp prometheus-2.21.0.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-2.21.0.linux-amd64/promtool /usr/local/bin/
+sudo chown prometheus:prometheus /usr/local/bin/prometheus
+sudo chown prometheus:prometheus /usr/local/bin/promtool
+sudo cp -r prometheus-2.21.0.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-2.21.0.linux-amd64/console_libraries /etc/prometheus
+sudo chown -R prometheus:prometheus /etc/prometheus/consoles
+sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+rm -rf prometheus-2.21.0.linux-amd64.tar.gz prometheus-2.21.0.linux-amd64
+sudo cat <<EOF >/etc/prometheus/prometheus.yml
+global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: 'prometheus'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['localhost:9090']
+EOF
+sudo chown prometheus:prometheus /etc/prometheus/prometheus.yml
+sudo cat <<EOF >/etc/systemd/system/prometheus.service
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl start prometheus
+sudo systemctl enable prometheus
